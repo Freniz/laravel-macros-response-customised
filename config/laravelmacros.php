fc0852e1e1ccf38d96967e0cb8752a0112a9d567
abdulnizam <abdulnizam89@gmail.com>
2018-01-28 10:35:47 +0400

    updated the config

--- config/laravelmacros.php
+++ config/laravelmacros.php
@@ -2,9 +2,11 @@
 
 return [
 
-  'multiple' => false,
+// example input for custom should be an array [ 1, 'message']
+// 'custom' => array('status', 'message')
+  
+  'custom' => array('message') //default
 
-  'custom' => array()
 
 
 ];
